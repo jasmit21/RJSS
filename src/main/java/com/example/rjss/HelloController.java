9c16a932e63642687a02107a6a0d0dc1f54837c1
jasmit21 <jasmitrathod21@gmail.com>
2021-11-30 02:25:04 +0530

    emi calculator

--- src/main/java/com/example/rjss/HelloController.java
+++ src/main/java/com/example/rjss/HelloController.java
@@ -6,9 +6,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import java.lang.Math;
 
 import java.io.IOException;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.Objects;
 
 public class HelloController {
@@ -61,16 +65,19 @@ public class HelloController {
         stage.show();
     }
 
-//    @FXML
-//    public void onHomeButtonClick(ActionEvent event) throws IOException {
-//        Parent root = FXMLLoader.load(getClass().getResource("home.fxml"));
-//        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-//        scene = new Scene(root);
-//        stage.setScene(scene);
-//        stage.show();
-//    }
+ @FXML
+    private TextField principalAmt, tenure, result;
+    NumberFormat formatter = new DecimalFormat("#0.00");
 
+@FXML
+    protected void onCalculateButtonClick(){
+    double amt = Double.parseDouble(principalAmt.getText());
+    double years = Double.parseDouble(tenure.getText());
+    double emi = ((amt*(7.0/1200)*Math.pow(1+(7.0/1200) , years*12))/(Math.pow(1+(7.0/1200) ,years*12)-1));
+//    double payableInterest = amt*years*12*
+    result.setText(String.valueOf(formatter.format(emi)));
 
+}
 
 }
 
